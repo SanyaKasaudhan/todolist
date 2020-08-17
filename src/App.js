9253db419da568cf746a6bae2a420d3a20975e92
SanyaKasaudhan <kasaudhansanya@gmail.com>
2020-08-17 14:24:17 +0530

    animated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>TO DO LIST</h1>
+        <br/>
+        <input type="text" placeholder="Add a items" />
+          <button>+</button>
+          <ol>
+            <li></li>
+          </ol>
+      </div>
+     
     </div>
+
+    </>
   );
 }
 
